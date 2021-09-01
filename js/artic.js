d7186df24fec6f66b7d71eeefd22b223f55ba1f5
robeert005cls <robeert005@hotmail.com>
2021-09-01 18:18:48 -0500

    Cambios en ejercicio articulos

--- js/artic.js
+++ js/artic.js
@@ -5,6 +5,8 @@ var app = new Vue({
     priceTv: 980000,
     priceLav: 860000,
     arrayArtic:[],
+    nombArtic: "",
+    prArtic: "",
     cant: null,
     total: 0
 
@@ -13,14 +15,19 @@ var app = new Vue({
     tArticulo() {
       if (this.opcValor == "1") {
         this.total += this.cant * this.priceTv;
+        this.nombArtic="Televisor"
+        this.prArtic= this.priceTv;
+        
       } else {
         this.total += this.cant * this.priceLav;
+        this.nombArtic="Lavadora"
+        this.prArtic= this.priceLav;
 
       }
       this.agregarDetalle();
     },
     agregarDetalle(){
-      this.arrayArtic.push(this.total);
+      this.arrayArtic.push({nombre:this.nombArtic,cant:this.cant,precio:this.prArtic,total:this.total});
 
     }
   }
